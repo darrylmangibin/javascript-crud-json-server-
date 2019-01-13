f1d5965e519edebe8a02149f31eb823eb2613859
Darryl <darrylmangibin@gmail.com>
2019-01-13 17:45:17 +0800

    [UPDATE] - initializing the app files

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import { http } from './http';
+import { ui } from './UI';
 
 document.addEventListener('DOMContentLoaded', getPosts);
 
 function getPosts() {
 	http.get('http://localhost:3000/post')
-		.then(data => console.log(data))
+		.then((data) => {
+			ui.showPosts(data)
+		})
 		.catch(err => console.log(err))
 }
